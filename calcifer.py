4f2f517d623dd111dea3c7685535911a2e122027
pumpkinpai <pumpkinson@gmail.com>
2015-11-16 11:13:19 -0500

    Fleshing out basics

--- calcifer.py
+++ calcifer.py
@@ -1,6 +1,43 @@
-#/bin/python3
+#!/usr/bin/python3
+
+import os
+import time
+import yaml
+
+def getConf(whichConfig):
+    confFile = os.getcwd() + '/config/' + whichConfig + '.yaml'
 
 
+def initConf():
+    # todo: this bit to get conf files in the right place
+    # check ~/.calcifer for conf files and create them if needed
 
-if __name__ == "main':
+def furnaceStatus(status):
+    # start furnace
+        # open flue damper # check safety
+        # close thermostat switch
+    # check heater coil temp
+        # when @targetTemp is reached, return True
+
+
+def heat(room):
+    # turn off heat exchanger fan
+    # close all duct dampers
+        # or keep track of what's open
+    # open room damper
+    # turn on heat exchanger fan
+    # return True
+
+
+if __name__ == 'main':
+
+    # First run, create conf files
+    initConf()
+
+    conf = getConf('main')
+
+    while (True):
+        # Check for heat commands from Daisy
+
+
 
